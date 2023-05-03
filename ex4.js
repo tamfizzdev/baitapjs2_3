c0700b866b7067193644509c44ee8718e2bb4020
tamfizzdev <tamfizzdev@gmail.com>
2023-05-03 12:44:29 +0700

    baitap

--- ex4.js
+++
@@ -1,9 +0,0 @@
-function hinhchunhat() {
-    var dai = document.getElementById('chieuDai').value;
-    var rong = document.getElementById('chieuRong').value;
-
-    var chuVi = (dai  + rong) * 2;
-    var dienTich = dai * rong;
-    document.getElementById('chuVi').innerHTML = chuvi;
-    document.getElementById('dienTich').innerHTML = dienTich;
-}
